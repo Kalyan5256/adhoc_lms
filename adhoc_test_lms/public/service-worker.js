ccfb8cdca46f4e1e0bbaabbf5905e86a51aae92b
Sri Venkatesh Nadapana <nadapanasrivenkatesh1@gmail.com>
2026-06-02 18:47:38 +0530

    Apply CSP fix and add service worker

---
+++ adhoc_test_lms/public/service-worker.js
@@ -0,0 +1,27 @@
+// public/service-worker.js
+// Simple Service Worker using Workbox (if available) for caching static assets.
+// This file will be served at /service-worker.js.
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (url.origin === location.origin) {
+    event.respondWith(
+      caches.open('static-assets').then((cache) =>
+        cache.match(event.request).then((response) =>
+          response || fetch(event.request).then((networkResponse) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          })
+        )
+      )
+    );
+  }
+});
